8132940dca5a57ce6d56c555680af0258ac91e4c
sautedman <bowersd@ucla.edu>
2018-11-21 22:21:13 -0700

    core function

---
+++ quotes.py
@@ -0,0 +1,14 @@
+from lxml import html
+import requests
+import sys
+
+def last_close(symbol):
+    #departed from usual ban against one-time use variables, function-internal data shunting for readability
+    content = requests.get('https://finance.yahoo.com/quote/{}?p={}'.format(symbol, symbol))
+    parser = html.fromstring(content.text)
+    summary_table = parser.xpath('//div[contains(@data-test,"summary-table")]//tr')
+    return float(summary_table[0].xpath('.//td[contains(@class,"Ta(end)")]//text()')[0])
+
+if __name__ == "__main__":
+    for x in sys.argv[1:]:
+        print x, "{:9.2f}".format(last_close(x))
